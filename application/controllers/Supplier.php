bdbd9518b661b3beacff7aacba0c301bf69bc32e
Fadlian17 <alfansyahfadlian@gmail.com>
2020-01-29 08:54:04 +0700

    pos update

--- application/controllers/Supplier.php
+++ application/controllers/Supplier.php
@@ -1,11 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
+class Supplier extends CI_Controller {
 
 	
 	public function index()
 	{
-		$this->load->view('template','dashboard');
+		$this->load->view('template','supplier');
 	}
 }
