fc2185dc346c456eb956e785ef7d421f63e4899e
Fadlian17 <alfansyahfadlian@gmail.com>
2020-02-05 21:32:08 +0700

    update batas akses3

--- application/views/user/user_form_add.php
+++ application/views/user/user_form_add.php
@@ -8,45 +8,55 @@
               <a href="<?=site_url('user')?>" class="btn btn-warning btn-flat">
               <i class="fas fa-undo-alt"></i> Back
               </a>
-            </div>
+			</div>
+        </div>
 
             <div class="box-body">
                 <div class="row">
-                    <div class="col-md-4">
-                        <form action="">
-                            <div class="form-group">
+                    <div class="col-md-4 col-md-offset-4">
+						<?php //echo validation_errors(); ?>
+                        <form action="" method="post">
+                            <div class="form-group <?=form_error('fullname')? 'has-error' : null?>">
                                 <label>Name *</label>
-                                <input type="text" name="fullname" class="form-control" placeholder="Name">
+								<input type="text" name="fullname" class="form-control" value="<?=set_value('fullname')?>" placeholder="name">
+								<?=form_error('fullname')?>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group <?=form_error('username')? 'has-error' : null?>">
                                 <label>Username *</label>
-                                <input type="text" name="username" class="form-control" placeholder="username">
+								<input type="text" name="username" class="form-control" value="<?=set_value('username')?>" placeholder="username">
+								<?=form_error('username')?>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group <?=form_error('password')? 'has-error' : null?>">
                                 <label>Password *</label>
-                                <input type="password" name="username" class="form-control" placeholder="***">
+								<input type="password" name="password" class="form-control" value="<?=set_value('password')?>" placeholder="***">
+								<?=form_error('password')?>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group <?=form_error('passconf')? 'has-error' : null?>">
                                 <label>Password Confirmation *</label>
-                                <input type="password" name="username" class="form-control" placeholder="***">
+								<input type="password" name="passconf" class="form-control" value="<?=set_value('passconf')?>" placeholder="***">
+								<?=form_error('passconf')?>
                             </div>
                             <div class="form-group">
-                                <label>Address *</label>
-                                <textarea name="fullname" class="form-control"></textarea>
+                                <label>Address </label>
+								<textarea name="address" value="<?=set_value('address')?>" class="form-control"></textarea>
+								<?=form_error('address')?>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group <?=form_error('level')? 'has-error' : null?>">
                                 <label>Level*</label>
                                 <select name="level" class="form-control">
                                     <option value="">Pilih</option>
-                                    <option value="1">Admin</option>
-                                    <option value="2">Kasir</option>
-                                </select>
+                                    <option value="1" <?=set_value('level') == 1? "selected" :null?>>Admin</option>
+                                    <option value="2" <?=set_value('level') == 2? "selected" :null?>>Kasir</option>
+								</select>
+								<?=form_error('level')?>
                             </div>
                             <div class="form-group">
                                 <button type="submit" class="btn btn-success btn-flat">
-                                        <i class="fa fa-paper-line"></i>Save
+								<i class="fas fa-paper-plane"></i> Save
                                 </button>
-                                <button type="Reset" class="btn btn-danger btn-flat">Reset</button>
+                                <button type="Reset" class="btn btn-default btn-flat">
+								<i class="fas fa-step-backward"></i> Reset
+								</button>
                             </div>
                         </form>
                     </div>
