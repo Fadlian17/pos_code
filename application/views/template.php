3f5cba971fedf96d30c501db286d3f876d0a54e3
Fadlian17 <alfansyahfadlian@gmail.com>
2020-02-05 21:35:13 +0700

    update batas akses3

--- application/views/template.php
+++ application/views/template.php
@@ -83,11 +83,9 @@
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
-                </div>
-                <div class="pull-right">
-                  <a href="<?=site_url('auth/logout')?>" class="btn btn-default btn-flat">Sign out</a>
-                </div>
+									<a href="#" class="btn btn-default btn-flat">Profile</a>
+									<a href="<?=site_url('auth/logout')?>" class="btn btn-danger btn-flat">Sign out</a>
+								</div>
               </li>
             </ul>
 					</li>
